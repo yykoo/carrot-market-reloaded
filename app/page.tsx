09590b3dee8214f2721c481dfd109fad89af1b9b
yykoo <iksoub@gmail.com>
2024-05-12 18:47:19 +0900

    form invalid(peer)

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 h-screen lg:bg-cyan-100 xl:bg-purple-100 2xl:bg-blue-300 flex items-center justify-center p-5">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
-        <input type="text" className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow" placeholder="Search here.." />
-        <button className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10 bg-gradient-to-tr from-cyan-500 via-yellow-100 to-purple-600">Search</button>
+        <input type="email" className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow invalid:focus:ring-green-500 peer" placeholder="Search here.." required />
+        <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required..</span>
+        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10 peer-invalid:bg-red-100">Login</button>
       </div>
     </main>
   );
