ae4de8d04132b9886d0534a74662aad4df49d33a
yykoo <iksoub@gmail.com>
2024-05-13 23:24:59 +0900

    List and animation

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,18 @@ import { Input } from "postcss";
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 h-screen lg:bg-cyan-100 xl:bg-purple-100 2xl:bg-blue-300 flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100 has-[:invalid]:ring">
-        <input type="email" className="w-full rounded-full h-12 bg-gray-200 pl-5 ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow invalid:focus:ring-green-500 peer" placeholder="Search here.." required />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required..</span>
-        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium md:px-10 peer-invalid:bg-red-100">Login</button>
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        {["iksoeb","Me","You","yourself", ""].map((person, index) =>
+          // <div key={index} className="flex items-center gap-5 even:bg-cyan-100 odd:bg-gray-100 p-2.5 rounded-xl">
+          <div key={index} className="flex items-center gap-5 border-b-2 pb-5 last:pb-0 last:border-0">
+            <div className="size-10 bg-blue-400 rounded-full"/>
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:bg-gray-300 empty:animate-pulse">{person}</span>
+            <div  className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
