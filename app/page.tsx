338f54b33a23d58f7298c67ce9be4793edc9ec65
yykoo <iksoub@gmail.com>
2024-05-19 02:04:29 +0900

    첫화면

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
-import { Input } from "postcss";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="main">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
-        {["iksoeb","Me","You","yourself", ""].map((person, index) =>
-          // <div key={index} className="flex items-center gap-5 even:bg-cyan-100 odd:bg-gray-100 p-2.5 rounded-xl">
-          <div key={index} className="flex items-center group gap-5 border-b-2 pb-5 last:pb-0 last:border-0">
-            <div className="size-10 bg-blue-400 rounded-full"/>
-            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:bg-gray-300 empty:animate-pulse group-hover:text-red-500">{person}</span>
-            <div  className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
-              <span className="z-10">{index}</span>
-              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
-            </div>
-          </div>
-        )}
-        <a href="#">sss</a>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl ">당근</h1>
+        <h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
       </div>
-    </main>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/create-account"
+          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
