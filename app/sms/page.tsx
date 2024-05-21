ba6cba9be9940adaee84e71a81e65f76143497b7
yykoo <iksoub@gmail.com>
2024-05-21 21:37:21 +0900

    components & login form

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -2,29 +2,28 @@ import FormInput from "../components/form-inputs";
 import FormButton from "../components/form-btn";
 import SocialLogin from "../components/social-login";
 
-export default function Login() {
+export default function SMSLogin() {
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
-                <h1 className="text-2xl">안녕하세요!</h1>
-                <h2 className="text-xl">Log in with email and password</h2>
+                <h1 className="text-2xl">SMS Login</h1>
+                <h2 className="text-xl">Verify your phone number</h2>
             </div>
             <form className="flex flex-col gap-3">
                 <FormInput
-                    type="email"
-                    placeholder="Email"
+                    type="number"
+                    placeholder="Phone number"
                     required
                     errors={[""]} 
                     />
                 <FormInput
-                    type="password"
-                    placeholder="password"
+                    type="number"
+                    placeholder="Verifycation code"
                     required
                     errors={[""]} 
                     />
-                <FormButton loading={false} text="Create account" />
+                <FormButton loading={false} text="Verify" />
             </form>
-            <SocialLogin />
         </div>
     );
 }
