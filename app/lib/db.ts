55a3046d16b08a41b61af3b42ff1fcd777ceabb3
yykoo <iksoub@gmail.com>
2024-05-31 01:47:09 +0900

    prisma relation

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -3,14 +3,41 @@ import { PrismaClient } from "@prisma/client"
 const db = new PrismaClient();
 
 async function test() {
-    const user = await db.user.create({
+    const token = await db.sMSToken.create({
         data: {
-            username:"sd"
-        }
+            token: "223123s2",
+            user:{
+                connect:{
+                    id:1,
+                },
+            },
+        },
     })
+    console.log(token)
 }
 
-test()
+async function test2() {
+    const token = await db.sMSToken.findUnique({
+        where: {
+            id:1
+        },
+        include: {
+            user:true
+        }
+    })
+    console.log(token)
+}
+
+//test()
+test2()
+
+// async function test() {
+//     const user = await db.user.create({
+//         data: {
+//             username:"sd"
+//         }
+//     })
+// }
 
 export default db;
 
