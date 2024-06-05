f5dfbcced1f5b7316c1095a34515e9d420c9df2e
yykoo <iksoub@gmail.com>
2024-06-05 19:34:05 +0900

    middleware session 적용

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,33 @@
 import getSession from "@/lib/session";
 import { cookies } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { boolean } from "zod";
+
+interface Routes {
+    [key:string]: boolean
+}
+const publicOnlyUrls: Routes = {
+    "/": true,
+    "/login": true,
+    "/sms": true,
+    "/create-account": true,
+}
 
 export async function middleware(request: NextRequest) {
-    //console.log(request.cookies.getAll())
-    //console.log(cookies())
+    const exists = publicOnlyUrls[request.nextUrl.pathname]
     const session = await getSession()
-    console.log(session)
-
-    if(request.nextUrl.pathname === "/profile") {
-        // return Response.json({
-        //     error:" you are not allowed here!"
-        // })
-        return Response.redirect(new URL('/', request.url))
+    if(!session.id) {
+        if(!exists) {
+            return NextResponse.redirect(new URL("/", request.url))
+        }
+    } else {
+        if(exists) {
+            return NextResponse.redirect(new URL("/product", request.url))
+        }
     }
+}
+
+export const config = {
+    //matcher: ["/", "/profile", "/user/:path*"]
+    matcher: ["/((?!api|_next|static|_next/image|favicon.ico).*)"]
 }
