63440d2af591819a30c4e6a6d8b9e31cf2bc7b62
yykoo <iksoub@gmail.com>
2024-06-16 22:13:45 +0900

    product form 작성

---
+++ app/products/add/page.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import Button from "@/components/button";
+import Input from "@/components/input";
+import { PhotoIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
+import { uploadProduct } from "./action";
+
+export default function AddProduct() {
+    const [preview, setPreview] = useState("")
+    const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        //console.log(event.target.files)
+        const { target: {files}, } = event
+
+        if(!files)  return
+
+        const file = files[0]
+        const url = URL.createObjectURL(file)
+        setPreview(url)
+    }
+
+    return (
+        <div>
+            <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+                <label
+                htmlFor="photo"
+                style={{backgroundImage: `url(${preview})`}}
+                className="border-2 aspect-square flex items-center justify-center flex-col
+                         text-neutral-300 border-neutral-300 rounded-md border-dashed 
+                         cursor-pointer bg-center bg-cover">
+                {preview === "" ? <>
+                        <PhotoIcon className="w-20" />
+                        <div className="text-neutral-400 text-sm">사진을 추가해주세요.</div>
+                    </> : null }
+                </label>
+                <input
+                    onChange={onImageChange}
+                    type="file"
+                    id="photo"
+                    name="photo"
+                    accept="image/*"
+                    className="hidden"
+                />
+                <Input name="title" required placeholder="제목" type="text" />
+                <Input name="price" type="number" required placeholder="가격" />
+                <Input
+                    name="description"
+                    type="text"
+                    required
+                    placeholder="자세한 설명"
+                />
+                <Button text="작성 완료" />
+            </form>
+        </div>
+    )
+}
