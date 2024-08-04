c1bd6e455670145a1c1936f9d244f2b2c4fabb01
yykoo <iksoub@gmail.com>
2024-08-04 11:57:33 +0900

    채팅리스트 > 진행중

--- app/(tabs)/chat/page.tsx
+++ app/(tabs)/chat/page.tsx
@@ -1,4 +1,25 @@
-export default function Chat() {
+import db from "@/lib/db"
+import getSession from "@/lib/session"
+import { notFound } from "next/navigation"
+
+// export async function getChatList(userId: number) {
+//     const list = await db.message.groupBy({
+//         by: [`chatRoomId`],
+//         where: {
+//             userId
+//         }
+//     })
+// }
+
+export default async function Chat() {
+    // const session = await getSession()
+
+    // if(!session.id)
+    // {
+    //     return notFound()
+    // }
+
+
     return (
         <div>
             <h1 className="">Chat</h1>
